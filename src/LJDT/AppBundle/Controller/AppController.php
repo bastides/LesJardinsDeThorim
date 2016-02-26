3d396bfdd9f7fb0bdd70b61e688d321442f3ce6c
bastides <bastide.sebastien@gmail.com>
2016-02-26 14:04:44 +0100

    Mise en place du design du formulaire de contact

--- src/LJDT/AppBundle/Controller/AppController.php
+++ src/LJDT/AppBundle/Controller/AppController.php
@@ -7,6 +7,12 @@ namespace LJDT\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use LJDT\AppBundle\Entity\Contact;
+use LJDT\AppBundle\Form\ContactType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AppController extends Controller
 {
@@ -39,6 +45,24 @@ class AppController extends Controller
             'listProducts' => $listProducts,
             'listProfiles' => $listProfiles,
             'listGalleries' => $listGalleries
+        ));
+    }
+    
+    public function formAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->get('form.factory')->create(ContactType::class, $contact);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('ljdt_app_home'));
+        }
+        
+        return $this->render('::/App/_contact.html.twig', array(
+          'form' => $form->createView(),
         ));
     }
 }
---
+++ src/LJDT/AppBundle/Form/ContactType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LJDT\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ContactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('object', TextType::class, array('required' => false))
+            ->add('message', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Envoyer'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LJDT\AppBundle\Entity\Contact'
+        ));
+    }
+}
